ec329c822a16bdd2a4d96216aa40076708c01df2
David Huie <david@nationbuilder.com>
2013-08-07 20:51:52 -0700

    Problem 2

--- src/euler_math/euler_math.go
+++ src/euler_math/euler_math.go
@@ -1,1 +1,14 @@
 package euler_math
+
+func FibList(num uint) []uint {
+	var fib_list []uint = make([]uint, num)
+
+	fib_list[0] = 0
+	fib_list[1] = 1
+
+	for i := 2; i < len(fib_list); i++ {
+		fib_list[i] = fib_list[i-1] + fib_list[i-2]
+	}
+
+	return fib_list
+}
---
+++ src/euler_problems/problem2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "euler_math"
+
+func problem2() uint {
+	var sum uint
+
+	fib_list := euler_math.FibList(4000000)
+	for _, fib := range fib_list {
+		if fib > 4000000 {
+			break
+		}
+		if fib%2 == 0 {
+			sum += fib
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(problem2())
+}
